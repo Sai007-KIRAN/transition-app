8713759196a5b529591a942406fb75c5f18a4154
kiran <kiransaipendela@gmail.com>
2024-12-01 18:30:17 +0530

    commit message

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,41 @@
+// import React from "react";
+// import Tabs from "./components/Tabs/Tabs";
+// import CustomerDetails from "./components/CustomerDetails/CustomerDetails";
+// import ConversationSection from "./components/ConversationSection/ConversationSection";
+// import ActionPanel from "./components/ActionPanel/ActionPanel";
+// import Sidebar from "./components/Sidebar/Sidebar";
+// import Header from "./components/Header/Header";
+// import "./App.css"; // For styling
+
+// const App = () => {
+//   return (
+//     <div className="app-container">
+//       {/* Tabs Section */}
+
+//       <Header />
+
+//       <Tabs />
+
+//       <Sidebar />
+
+//       {/* Content Area */}
+//       <div className="content-area">
+//         {/* Customer Details Section */}
+//         <CustomerDetails />
+
+//         {/* Conversation Section */}
+//         <ConversationSection />
+
+//         {/* Action Panel Section */}
+//         <ActionPanel />
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default App;
+
+
 import React from "react";
 import Tabs from "./components/Tabs/Tabs";
 import CustomerDetails from "./components/CustomerDetails/CustomerDetails";
@@ -5,29 +43,33 @@ import ConversationSection from "./components/ConversationSection/ConversationSe
 import ActionPanel from "./components/ActionPanel/ActionPanel";
 import Sidebar from "./components/Sidebar/Sidebar";
 import Header from "./components/Header/Header";
-import "./App.css"; // For styling
+import "./App.css";
 
 const App = () => {
   return (
     <div className="app-container">
-      {/* Tabs Section */}
-
+      {/* Header */}
       <Header />
 
-      <Tabs />
+      <div className="main-content">
+        {/* Sidebar */}
+        <Sidebar />
 
-      <Sidebar />
+        <div className="main-section">
+          {/* Tabs */}
+          <Tabs />
 
-      {/* Content Area */}
-      <div className="content-area">
-        {/* Customer Details Section */}
-        <CustomerDetails />
+          <div className="content-area">
+            {/* Customer Details */}
+            <CustomerDetails />
 
-        {/* Conversation Section */}
-        <ConversationSection />
+            {/* Conversation Section */}
+            <ConversationSection />
 
-        {/* Action Panel Section */}
-        <ActionPanel />
+            {/* Action Panel */}
+            <ActionPanel />
+          </div>
+        </div>
       </div>
     </div>
   );
